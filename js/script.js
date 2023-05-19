2e067a54792d6846311f46197065c4dd7ed3751d
yonathanhadijayas <yonathanhs.exe@gmail.com>
2023-05-19 16:16:04 +0700

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+//klik diluar sidebar untuk hilangkan sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
